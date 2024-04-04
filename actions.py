c1f6594b0b3ed687dfa2ba5096cc5b3d9471e79b
Andres <rodriguezcotojoseandres@gmail.com>
2024-04-04 14:19:49 -0600

    Implemented winner and input validations

--- actions.py
+++ actions.py
@@ -9,14 +9,7 @@ class Actions:
         B {self.valueList[3]} {self.valueList[4]} {self.valueList[5]}  
         C {self.valueList[6]} {self.valueList[7]} {self.valueList[8]}"""
         self.winnerExist = True
-
-        
-    def printMenu(self):
-        print("Please select the option where you want to type")
-        print(self.ticTacToe)
-    
-    def validateInputs(self, inputPlayer1, inputPlayer2):
-        positions = {
+        self.positions = {
             'aa': 0,
             'ab': 1,
             'ac': 2,
@@ -27,11 +20,19 @@ class Actions:
             'cb': 7,
             'cc': 8
         }
+
+        
+    def printMenu(self):
+        print("Please select the option where you want to type")
+        print(self.ticTacToe)
+    
+    def validateInputs(self, inputPlayer1, inputPlayer2):
+
         if inputPlayer1 == inputPlayer2:
             print("Players cannot have the same input, please choose again")
         elif inputPlayer1 != inputPlayer2:
             for inputPlayer, marker in [(inputPlayer1, 'X'), (inputPlayer2, 'Y')]:
-                position = positions.get(str(inputPlayer).lower())
+                position = self.positions.get(str(inputPlayer).lower())
                 if position is not None:
                     self.valueList[position] = marker
             self.setMenu(self.valueList)
@@ -39,19 +40,19 @@ class Actions:
         else:
             self.setWinnerExist(False)
     
-    def check_horizontal(valueList, symbol):
+    def check_horizontal(self, valueList, symbol):
         for i in range(0, len(valueList), 3):
             if valueList[i:i+3] == [symbol] * 3:
                 return True
         return False
 
-    def check_vertical(valueList, symbol):
+    def check_vertical(self, valueList, symbol):
         for i in range(3):
             if valueList[i] == valueList[i+3] == valueList[i+6] == symbol:
                 return True
         return False
 
-    def check_diagonal(valueList, symbol):
+    def check_diagonal(self, valueList, symbol):
         if valueList[0] == valueList[4] == valueList[8] == symbol:
             return True
         if valueList[2] == valueList[4] == valueList[6] == symbol:
@@ -59,7 +60,13 @@ class Actions:
         return False
             
     def checkWinner(self, valueList):
-        pass
+        if (self.check_horizontal(valueList, 'X') or self.check_diagonal(valueList, 'X') or self.check_vertical(valueList, 'X')):
+            print('Player 1 wins!!')
+            self.setWinnerExist(False)
+        elif (self.check_horizontal(valueList, 'Y') or self.check_diagonal(valueList, 'Y') or self.check_vertical(valueList, 'Y')):
+            print('Player 2 wins!!')
+            self.setWinnerExist(False)
+        
 
     def setMenu(self, updatedList):
         self.ticTacToe = f"""  
@@ -74,19 +81,41 @@ class Actions:
     def setWinnerExist(self, value):
         self.winnerExist = value
         
-    def getPlayer1input(self):
+    def getPlayer1input(self):        
         return self.player1Input
         
     def setPlayer1input(self):
         print("Player 1 please type your input")
-        self.player1Input = input()
-        self.player1Input.upper()
+        validInput = False  
+
+        while not validInput:
+            currentInput = input()
+            currentInput.lower()
+            
+            if currentInput not in self.positions:  
+                print("Invalid input. Please try again.")
+            else:
+                currentInput.upper()
+                self.player1Input = currentInput
+                validInput = True  
+
+        
         
     def getPlayer2input(self):
         return self.player2Input
         
     def setPlayer2input(self):    
         print("Player 2 please type your input")
-        self.player2Input = input()
-        self.player1Input.upper()
+        validInput = False  
+
+        while not validInput:
+            currentInput = input()
+            currentInput.lower()
+            
+            if currentInput not in self.positions:  
+                print("Invalid input. Please try again.")
+            else:
+                currentInput.upper()
+                self.player2Input = currentInput
+                validInput = True  
     
